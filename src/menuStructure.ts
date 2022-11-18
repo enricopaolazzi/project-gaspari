f6f7a4b949ec49c0fd871dfcaa84d2ea9f46dd0f
Enrico <enrico.paolazzi@mycity.it>
2022-11-18 17:35:04 +0100

    dfrgtyhu78i

--- src/menuStructure.ts
+++ src/menuStructure.ts
@@ -11,49 +11,42 @@ const menuStructure : any = [
             },
             {
                 id: 12,
-                href: '/about',
+                href: "/about",
                 label: 'Aree',
                 children: []
             },
             {
                 id: 13,
-                href: 'ghejhce',
                 label: 'Uffici',
                 children: []
             },
             {
                 id: 14,
-                href: 'ghejhce',
                 label: 'Contatti',
                 children: []
             },
             {
                 id: 15,
-                href: 'ghejhce',
                 label: 'Localizzazioni',
                 children: []
             },
             {
                 id: 16,
-                href: 'ghejhce',
                 label: 'Fornitori',
                 children: []
             },
             {
                 id: 17,
-                href: 'ghejhce',
                 label: 'Agenti',
                 children: []
             },
             {
                 id: 18,
-                href: 'ghejhce',
                 label: 'Depositi',
                 children: []
             },
             {
                 id: 19,
-                href: 'ghejhce',
                 label: 'Catalogo',
                 children: []
             },            
@@ -61,54 +54,45 @@ const menuStructure : any = [
     },
     {
         id: 2,
-        href: 'ghejhce',
         label: 'Operatività',
         children: [
             {
                 id: 21,
-                href: 'ghejhce',
                 label: 'Richieste agenti',
                 children: []
             },
             {
                 id: 22,
-                href: 'ghejhce',
                 label: 'Preventivi',
                 children: []
             },
             {
                 id: 23,
-                href: 'ghejhce',
                 label: 'Preventivi Speciali',
                 children: []
             },
             {
                 id: 24,
-                href: 'ghejhce',
                 label: 'Ordini',
                 children: []
             },
             {
                 id: 25,
-                href: 'ghejhce',
                 label: 'Licenze',
                 children: []
             },
             {
                 id: 26,
-                href: 'ghejhce',
                 label: 'Commesse (task)',
                 children: []
             },
             {
                 id: 27,
-                href: 'ghejhce',
                 label: 'Bolle e DDT',
                 children: []
             },
             {
                 id: 28,
-                href: 'ghejhce',
                 label: 'Fatture',
                 children: []
             },
@@ -116,48 +100,40 @@ const menuStructure : any = [
     },
     {
         id: 3,
-        href: 'ghejhce',
         label: 'Magazzino',
         children: []
     },
     {
         id: 4,
-        href: 'ghejhce',
         label: 'Finanza e Contabilità',
         children: []
     },
     {
         id: 5,
-        href: 'ghejhce',
         label: 'Statistiche & Marketing',
         children: [
             {
                 id: 51,
-                href: 'ghejhce',
                 label: 'Statistiche di vendita',
                 children: []
             },
             {
                 id: 52,
-                href: 'ghejhce',
                 label: 'Newsletters',
                 children: []
             },
             {
                 id: 53,
-                href: 'ghejhce',
                 label: 'Promozioni',
                 children: []
             },
             {
                 id: 54,
-                href: 'ghejhce',
                 label: 'Gestione Sito Gaspari',
                 children: []
             },
             {
                 id: 55,
-                href: 'ghejhce',
                 label: 'Gestione E-Commerce',
                 children: []
             }
@@ -165,30 +141,25 @@ const menuStructure : any = [
     },
     {
         id: 6,
-        href: 'ghejhce',
         label: 'Varie',
         children: [
             {
                 id: 61,
-                href: 'ghejhce',
                 label: 'Documenti',
                 children: []
             },
             {
                 id: 62,
-                href: 'ghejhce',
                 label: 'Notifiche',
                 children: []
             },
             {
                 id: 63,
-                href: 'ghejhce',
                 label: 'E-mails',
                 children: []
             },
             {
                 id: 64,
-                href: 'ghejhce',
                 label: 'Templates',
                 children: []
             }
